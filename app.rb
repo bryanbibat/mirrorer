e0281a8f2391b1d4e5754e43c8f3b5b498699d86
Bryan Bibat <bryan@aelogica.com>
2015-10-08 09:23:25 +0800

    WIP

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require 'sinatra/base'
 require 'json'
+require 'fileutils'
+require 'open3'
 
 class Mirrorer < Sinatra::Base
 
@@ -8,10 +10,23 @@ class Mirrorer < Sinatra::Base
   end
 
   post '/' do
-    puts "request: #{request.env['HTTP_X_GITHUB_EVENT']}"
-    puts "body: #{body}"
-    if body.length > 1
-      puts JSON.parse(body).inspect
+    if request.env['HTTP_X_GITHUB_EVENT'] == "push"
+      req_body = request.body.read
+      puts "body: #{req_body}"
+      if body.length > 1
+        payload = JSON.parse(payload)
+        puts payload.inspect
+        full_name = payload["repository"]["full_name"]
+        unless Dir.exists?(File.join("builds", full_name))
+          Open3.popen3("git clone https://github.com/#{full_name}.git builds/#{full_name}")
+
+        end
+        if payload["ref"] == "refs/heads/tags"
+
+        else
+
+        end
+      end
     end
     'Hello world!'
   end
