51524930a89ee3c820108521cf9863af3d5dbcf4
Bryan Bibat <bry@bryanbibat.net>
2015-10-08 08:32:41 +0800

    Check first before parsing

--- app.rb
+++ app.rb
@@ -4,7 +4,10 @@ require 'json'
 class Mirrorer < Sinatra::Base
 
   get '/' do
-    puts JSON.parse(request.body.read).inspect
+    body = request.body.read
+    if body.length > 1
+      puts JSON.parse(request.body.read).inspect
+    end
     'Hello world!'
   end
 
